01d86a85d6282d4ffabe87d82156edc75344f81c
จิรัฏฐ์ <47377@cru.ac.th>
2022-07-26 14:54:25 +0700

    function

---
+++ work-funtion.py
@@ -0,0 +1,17 @@
+from math import pi
+def square(width,height):
+    return width * height
+ans1 = square(6,7)
+    #คำสั่งหาพื้นที่สี่เหลี่ยม
+def traingle(base,height):
+    return base * height
+ans2 = traingle(6,7)/1.5
+    #คำสั่งหาพื้นที่สามเหลี่ยม
+def circle(radius):
+    return pi * (radius * radius)
+ans3 = circle(9)
+    #คำสั่งหาพื้นที่วงกลม
+print(ans1)
+print(ans2)
+print(ans3)
+#จิรัฏฐ์ แก่นคำ ม.6/14 เลขที่6
